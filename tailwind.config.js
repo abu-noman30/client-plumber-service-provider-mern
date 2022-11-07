741e8ff39520b0220cfc905acdc758a401e23ba2
noman <nomannabil30@gmail.com>
2022-11-07 22:29:32 +0600

    first commit- client sides necessary packages installed

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+	],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#6382dd',
+
+					secondary: '#bcd83e',
+
+					accent: '#8ebbdb',
+
+					neutral: '#2C343A',
+
+					'base-100': '#E8EAED',
+
+					info: '#46AEE2',
+
+					success: '#207E65',
+
+					warning: '#F0D175',
+
+					error: '#F53858'
+				}
+			}
+		]
+	},
+	plugins: [require('daisyui'), require('flowbite/plugin')]
+};
